f87ef7dcb8d93d4b43aa4b7290e1199f83e3560a
rasmusWU <62059075+rasmusWU@users.noreply.github.com>
2020-12-16 08:58:41 +0100

    auth added

--- cheese.route.js
+++ cheese.route.js
@@ -1,8 +1,9 @@
 var Cheese = require("./cheese.model");
+var auth = require("./auth-middleware");
 
 module.exports = function (app) {
     //create a cheesse
-    app.post("/api/v1/cheeses", function(request, response, next) {
+    app.post("/api/v1/cheeses", auth, function(request, response, next) {
         try {
             var cheese = new Cheese({
                 name: request.fields.name,
@@ -22,19 +23,44 @@ module.exports = function (app) {
 
     // get all cheeses
     app.get("/api/v1/cheeses", async function (request, response, next) {
+        var limit = parseInt(request.query.limit) || 5;
+        var offset = parseInt(request.query.offset) || 0;
+
         try {
-            var result = await Cheese.find();
+            var result = await Cheese.find().limit(limit).skip(offset);
+            var count = (await Cheese.find()).length;
+
+            // ["limit=5", "offset=2"].join("&")
+
+            var qLimit = request.query.limit;
+            var qOffset = request.query.offset || 0;
+
+            var queryStringNext = [];
+            var queryStringPrevious = [];
+
+            if (qLimit) {
+                queryStringNext.push("limit=" + qLimit);
+                queryStringPrevious.push("limit=" + qLimit);
+            }
+
+            if (qOffset) {
+                queryStringNext.push("offset=" + parseInt(qOffset) + limit);
+                queryStringPrevious.push("offset=" + parseInt(qOffset) - limit);
+            }
+
+            var baseUrl = `${request.protocol}://${request.hostname}${ request.hostname == "localhost" ? ":" + process.env.PORT : ""}${ request._parsedUrl.pathname }`
+
+            console.log(offset + limit > count);
 
             var output = {
-                count: result.length,
-                next: `${request.protocol}://${request.hostname}${ request.hostname == "localhost" ? ":" + process.env.PORT : ""}${ request.url }?offset=20`,
-                previous: "null",
-                url: `${request.protocol}://${request.hostname}${ request.hostname == "localhost" ? ":" + process.env.PORT : ""}${ request.url }`,
+                count,
+                next: (offset + limit > count) ? `${baseUrl}?${queryStringNext.join("&")}` : null,
+                previous: offset > 0 ? `${baseUrl}?${queryStringNext.join("&")}` : null,
+                url: `${baseUrl}?` + (offset ? "offset=" + offset : ""),
                 results: result
             }
 
-            response.json(result);
-
+            response.json(output);
         } catch (error) {
             return next(error);
         }
@@ -63,7 +89,7 @@ module.exports = function (app) {
     });
 
     // update a cheese
-    app.patch("/api/v1/cheeses/:id", async function(request, response, next) {
+    app.patch("/api/v1/cheeses/:id", auth, async function(request, response, next) {
         try {
             var { name, price, weight, strength, brand } = request.fields;
             var updateObject = {};
@@ -87,7 +113,7 @@ module.exports = function (app) {
     });
 
     // delete a single cheese by id
-    app.delete("/api/v1/cheese/:id", async function(request, response, next) {
+    app.delete("/api/v1/cheese/:id", auth, async function(request, response, next) {
         try {
             await Cheese.findByIdAndRemove(request.params.id);
             response.status(204);
